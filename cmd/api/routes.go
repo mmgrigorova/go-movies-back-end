2b32d2a2c424e04e6c5f55663ac07c40754e5678
Maria Grigorova <mgrigorova@synchr.com>
2022-12-21 18:10:50 +0200

    Backend layout

---
+++ cmd/api/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+// every var app (the receiver) of type application will have access to that function
+// no params, returns http handler
+func (app *application) routes() http.Handler {
+	// create a router mux
+	mux := chi.NewRouter()
+
+	//middleware
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", app.Home)
+
+	mux.Get("/movies", app.AllMovies)
+
+	return mux
+}
