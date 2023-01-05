d8283b2b6c680e243957bb0edf8eda11820a30e3
Maria Grigorova <mgrigorova@synchr.com>
2023-01-05 17:58:56 +0200

    Create DB Repository

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +13,8 @@ const port = 8081
 
 type application struct {
 	Domain string
+	DSN    string // data source name
+	DB     repository.DatabaseRepo
 }
 
 func main() {
@@ -17,15 +22,25 @@ func main() {
 	var app application
 
 	// read from command line
+	// connection string
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5433 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
